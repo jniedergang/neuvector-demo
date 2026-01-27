212d50d3a503fffb23e2f8f780db5e0cf620cda2
root <root@admin2.home.lo>
2026-01-27 17:09:56 +0100

    refactor: Remove Security Policy Mode demo

--- app/demos/__init__.py
+++ app/demos/__init__.py
@@ -5,6 +5,5 @@ from app.demos.base import DemoModule
 # Import demos to trigger registration
 from app.demos import connectivity
 from app.demos import dlp
-from app.demos import security_mode
 
 __all__ = ["DemoRegistry", "DemoModule"]
